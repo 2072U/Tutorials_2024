a6d50419fbc4c1250b23fd38df9fbb7f7170230f
lvanveen <lennaert.vanveen@ontariotechu.ca>
2024-01-15 09:28:07 -0500

    Added starter code and PDF for tutorial 1.

---
+++ 01/Tut_01_A_start.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def FP(x, a, kMax, eps):
+
+    ...
+
+    return x,err
+
---
+++ 01/Tut_01_B_start.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def FP(x, a, kMax, epsX, epsF):
+
+    ...
+
+    return x,err,res
+
